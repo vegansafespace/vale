deb54372e50598d71e3a27ec367e098e216e2af1
Nick <nickreynke@gmail.com>
2023-12-30 18:25:11 +0100

    feat(commands): update commands to hybrid and move role check decorator
    The commands were previously using the standard command decorator, but these have now been converted into hybrid commands. Furthermore, the decorator checking for a team role has been moved directly underneath the hybrid command decorator instead of being placed later in the code. This helps in understanding the code's intent more clearly.

--- src/cogs/team_utils.py
+++ src/cogs/team_utils.py
@@ -14,13 +14,13 @@ class TeamUtils(commands.Cog):
     def __init__(self, bot: Vale):
         self.bot = bot
 
-    @commands.command()
+    @commands.hybrid_command()
+    @commands.has_role(TEAM_ROLE_ID)
     @app_commands.describe(
         user=f'Die Person, die gebannt werden soll',
         reason=f'Der Grund warum die Person gebannt werden soll',
         delete_messages=f'Ob die Nachrichten der Person gelöscht werden sollen'
     )
-    @app_commands.checks.has_role(TEAM_ROLE_ID)
     async def ban(self, interaction: discord.Interaction, user: discord.User, reason: str,
                   delete_messages: bool = False):
         executor = interaction.user
@@ -74,12 +74,12 @@ class TeamUtils(commands.Cog):
             user.mention,
         ), ephemeral=True)
 
-    @commands.command()
+    @commands.hybrid_command()
+    @commands.has_role(TEAM_ROLE_ID)
     @app_commands.describe(
         member='Eine Person die die Vegan-Rolle bekommen soll',
         reason='Der Grund warum die Person die Vegan-Rolle bekommen soll',
     )
-    @app_commands.checks.has_role(TEAM_ROLE_ID)
     async def vegan(self, ctx: commands.Context, member: discord.Member, reason: str):
         vegan_role = discord.utils.get(ctx.interaction.guild.roles, id=VEGAN_ROLE_ID)
         non_vegan_role = discord.utils.get(ctx.interaction.guild.roles, id=NON_VEGAN_ROLE_ID)
